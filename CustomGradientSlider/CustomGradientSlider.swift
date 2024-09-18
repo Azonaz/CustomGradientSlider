73aa89658dca2d21049f899704327cc0843e5397
Azonaz <malinina.a@gmail.com>
2024-09-18 14:54:18 +0300

    Custom gradient slider

---
+++ CustomGradientSlider/CustomGradientSlider.swift
@@ -0,0 +1,8 @@
+//
+//  a.swift
+//  CustomGradientSlider
+//
+//  Created by Админ on 18/09/2024.
+//
+
+import Foundation
--- CustomGradientSlider/GradintSliderViewController.swift
+++ CustomGradientSlider/GradintSliderViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GradintSliderViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
